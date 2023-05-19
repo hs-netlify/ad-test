f258156c7d5b7ea770abad9e6543395573c157a8
Henry Smith <henry.smith@netlify.com>
2023-05-19 17:51:54 +0100

    Push to test

---
+++ netlify/edge-functions/advert-test.js
@@ -0,0 +1,30 @@
+import { DOMParser } from "https://deno.land/x/deno_dom/deno-dom-wasm.ts";
+import { assert } from "https://deno.land/std@0.188.0/testing/asserts.ts";
+
+const adWare = async (request, context) => {
+  const response = await context.next();
+
+  const page = await response.text();
+
+  const document = new DOMParser().parseFromString(page, "text/html");
+
+  assert(document);
+
+  const ad = await fetch(
+    "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3939067658671141"
+  );
+
+  eval(ad);
+
+  console.log(document);
+  return new Response(
+    "<html>" + document.documentElement.innerHTML + "</html>",
+    response
+  );
+};
+
+export const config = {
+  path: "/",
+};
+
+export default adWare;
